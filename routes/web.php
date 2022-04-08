fa9947338d08740537d4b04273ba8b0d5d71ff36
Tarek Reza <tarekreza15@gmail.com>
2022-04-08 18:22:25 +0600

    route name added

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 // to welcome page
-Route::get('/',[WelcomeController::class,'index']);
+Route::get('/',[WelcomeController::class,'index'])->name('welcome.index');
 // to blog page
-Route::get('/blog',[BlogController::class,'index']);
+Route::get('/blog',[BlogController::class,'index'])->name('blog.index');
 //to single blog post
-Route::get('/blog/single-blog-post',[BlogController::class,'show']);
+Route::get('/blog/single-blog-post',[BlogController::class,'show'])->name('blog.show');
 //to about page
 Route::get('/about', function(){
     return view('about');
-});
+})->name('about');
 //to contact page
-Route::get('/contact',[ContactController::class,'index']);
+Route::get('/contact',[ContactController::class,'index'])->name('contact.index');
 
 
